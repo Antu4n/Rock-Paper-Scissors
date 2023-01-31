eb7714029a372437ac835a30d2415ec6f6af24de
Antony Githinji <githinji.antony.agm@gmail.com>
2023-01-31 23:00:35 +0300

    Added a simple GUI with Javascript

--- js.js
+++ js.js
@@ -5,59 +5,129 @@ function getComputerChoice(){
     return choice;
 }
 
-//check for win or lose 
 let playerCount = 0;
 let computerCount = 0;
+function playRound(computerSelection){      
+   
+    let playerSelection = this.id;
+    if (playerSelection == 'paper'){
 
-function playRound(playerSelection, computerSelection){    
-    playerSelection = prompt("Rock,Paper,Scissors? ");
-    playerSelection = playerSelection.toLowerCase();
+        playerChoice.style.fontSize = '2.4em';
+        playerChoice.innerHTML = 'PLAYER' +'<br>' +'<i class="fa-regular fa-hand "></i>';
+
+    }
+    else if ( playerSelection == 'rock'){
+        playerChoice.style.fontSize = '2.4em'; 
+        playerChoice.innerHTML = "PLAYER" +'<br>' + '<i class="fa-regular fa-hand-back-fist "></i>';
+    } 
+
+    else if(playerSelection == 'scissors'){
+        playerChoice.style.fontSize = '2.4em';
+        playerChoice.innerHTML  = "PLAYER"  +'<br>' +'<i class="fa-regular fa-hand-scissors "></i>';
+
+    }
     computerSelection = getComputerChoice();
+    if (computerSelection == 'paper'){
 
-    if (computerSelection == playerSelection){
-            return ['TIE!', computerCount ,playerCount]
+        choice.innerHTML = 'COMPUTER ' +'<br>' +'<i class="fa-regular fa-hand "></i>';
+        choice.style.fontSize =' 2.4em';
     }
-    else if (computerSelection == 'rock' && playerSelection == 'scissors'){
+    else if (computerSelection == 'rock'){
+        choice.innerHTML = 'COMPUTER ' +'<br>' + '<i class="fa-regular fa-hand-back-fist "></i>';
+        choice.style.fontSize =' 2.4em';     
+    } 
+
+    else if(computerSelection == 'scissors'){
+        choice.innerHTML = 'COMPUTER ' +'<br>' +'<i class="fa-regular fa-hand-scissors "></i>';
+        choice.style.fontSize =' 2.4em';       
+    }
+    
+    if (computerSelection === playerSelection){
+        playerCount = playerCount;
+        computerCount  = computerCount;
+        output("Tie " + `${playerSelection}`+ " ties " + `${computerSelection}`);
+           
+    }
+    else if (computerSelection === 'rock' && playerSelection === 'scissors'){
         computerCount++;
-        return ['You Lose! Rock beats Scissors', computerCount, playerCount]
+        playerCount = playerCount;
+        output("You lose! " + computerSelection + " beats " + playerSelection);
+               
     }
-    else if(computerSelection == 'paper' && playerSelection == "scissors"){
+    else if(computerSelection === 'paper' && playerSelection === "scissors"){
         playerCount++; 
-        return ["You win! Scissors beats paper", computerCount, playerCount]
+        computerCount = computerCount
+        output("You WIN! " + playerSelection + " beats "+ computerSelection);
     }
-    else if(computerSelection == 'scissors' && playerSelection == "paper"){
+    else if(computerSelection === 'scissors' && playerSelection === "paper"){
         computerCount++;
-        return ["You lose! Scissors beats paper", computerCount, playerCount]
+        playerCount = playerCount;
+        output("You lose! " + computerSelection + " beats " + playerSelection);
+        
     }
-    else if(computerSelection == 'scissors' && playerSelection == 'rock'){
+    else if(computerSelection === 'scissors' && playerSelection === 'rock'){
         playerCount++;
-        return[ "You win! Rock beats Scissors", computerCount, playerCount]
+        computerCount = computerCount; 
+        output("You WIN! " + playerSelection + " beats "+ computerSelection);
+
+       
     }
-    else if (computerSelection == "paper" && playerSelection == 'rock'){
+    else if (computerSelection === "paper" && playerSelection === 'rock'){
         computerCount++;
-        return ["You lose! Paper beats rock", computerCount, playerCount]
+        playerCount = playerCount;
+        output("You lose! " + computerSelection + " beats " + playerSelection);
     }
-    else if (computerSelection == 'rock' && playerSelection == 'paper'){
+    else if (computerSelection ==='rock' && playerSelection === 'paper'){
         playerCount++;
-        return ["You win! Paper beats rock" , computerCount, playerCount]
+        computerCount = computerCount;
+        output("You WIN! " + playerSelection + " beats "+ computerSelection);
+    }  
+}
+
+function result(){
+    playerScore.textContent = "|" + playerCount + ":";
+    compScore.textContent = computerCount + "|";
+ }
+function output (output){
+    outcome.textContent = output;
+    checkWin();
+}
+function checkWin(){
+    if(computerCount == 5){
+        game.textContent = "YOU LOSE THE GAME :("
+        playerCount = 0;
+        computerCount = 0;    
+        play.style.display = "none";
+        outcome.style.display = 'none';
+
     }
-    else{
-        computerCount++
-        return ["Wrong input! You Lose" , computerCount, playerCount]
+    else if (playerCount == 5){
+        game.textContent =  "YOU WIN THE GAME :D";
+        playerCount = 0;
+        computerCount = 0;   
+        play.style.display = 'none'
+        outcome.style.display = 'none';
+
     }
 }
-function game(){
-    for (let i = 0; i < 5; i++){
-        console.log(playRound());
-    }
-    if (computerCount > playerCount){
-        console.log("You lose :(")
-    }
-    else if(computerCount < playerCount){
-        console.log ("YOU WIN!!!");
-    }
-    else{
-       console.log("TIE");
-    }
+function restart(){
+    location.reload();
 }
-game();
+
+const restartBtn = document.querySelector('.restart')
+restartBtn.addEventListener('click',restart);
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {    
+    button.addEventListener('click', playRound)
+    button.addEventListener('click',result) 
+});
+
+const score = document.querySelector('#score');
+const playerScore = document.querySelector('#playerScore');
+const compScore = document.querySelector('#compScore');
+const outcome = document.querySelector('#outcome');
+const choice = document.querySelector('#compChoice');
+const game = document.querySelector('#result');
+const playerChoice = document.querySelector('#playerChoice');
+const play = document.querySelector('#game');
